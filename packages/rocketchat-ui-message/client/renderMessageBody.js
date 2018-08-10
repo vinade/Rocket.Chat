6cddddc719ae10d47c0cf130d0cab3b4301e8bbc
Tasso Evangelista <tasso@tassoevan.me>
2018-08-10 16:26:33 -0300

    Check tokens property in message before render it

--- packages/rocketchat-ui-message/client/renderMessageBody.js
+++ packages/rocketchat-ui-message/client/renderMessageBody.js
@@ -4,8 +4,8 @@ import s from 'underscore.string';
 renderMessageBody = message => {
 	message.html = s.trim(message.msg) ? s.escapeHTML(message.msg) : '';
 	const renderedMessage = RocketChat.callbacks.run('renderMessage', message);
-	return renderedMessage.tokens.reverse()
-		.reduce((html, { token, text }) => html.replace(token, () => text), renderedMessage.html);
+	const tokens = (renderedMessage.tokens && renderedMessage.tokens.reverse()) || [];
+	return tokens.reduce((html, { token, text }) => html.replace(token, () => text), renderedMessage.html);
 };
 
 /* exported renderMessageBody */
